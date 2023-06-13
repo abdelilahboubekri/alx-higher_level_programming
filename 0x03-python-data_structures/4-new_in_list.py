0eeb90a0dac1576ea92a6804a1dcf4734b21733f
abdelilahboubekri <abdelilahbob@gmail.com>
2023-06-12 18:18:29 -0700

    py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""function that replaces an element in a list at a specific position 
+"""function that replaces an element in a list at a specific position
 without modifying the original list (like in C)."""
+
 
 def new_in_list(my_list, idx, element):
 
