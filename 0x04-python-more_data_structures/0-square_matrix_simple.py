733c286ab0c360a6f0c78b8c6482de65ca0516f8
abdelilahboubekri <abdelilahbob@gmail.com>
2023-06-14 19:06:28 -0700

    py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+""""Function that computes the square value of all integers of a matrix."""
+
+
 def square_matrix_simple(matrix=[]):
+
     new_matrix = matrix.copy()
 
     for i in range(len(matrix)):
