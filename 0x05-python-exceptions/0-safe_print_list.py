8950f76cf977b6989216c56cf43939baa65a21aa
abdelilahboubekri <abdelilahbob@gmail.com>
2023-06-26 17:49:57 -0700

    intger

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+
+    try:
+        number = 0
+        for i in range(x):
+            print(my_list[i], end="")
+            number = number + 1
+            for x in range(number):
+                print("", end="")
+        print()
+        return number
+
+    except IndexError:
+
+        print()
+        return number
