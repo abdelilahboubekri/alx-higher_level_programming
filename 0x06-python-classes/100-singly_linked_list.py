f85e87edf526aa1669549c087e0d6e76714ec2c6
abdelilahboubekri <abdelilahbob@gmail.com>
2023-06-29 17:48:22 -0700

    sqr

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -63,7 +63,8 @@ class SinglyLinkedList:
             self.__head = new
         else:
             tmp = self.__head
-            while (tmp.next_node is not None and tmp.next_node.data < value):
+            while (tmp.next_node is not None and
+                    tmp.next_node.data < value):
                 tmp = tmp.next_node
             new.next_node = tmp.next_node
             tmp.next_node = new
@@ -75,3 +76,4 @@ class SinglyLinkedList:
         while tmp is not None:
             values.append(str(tmp.data))
             tmp = tmp.next_node
+        return ('\n'.join(values))
