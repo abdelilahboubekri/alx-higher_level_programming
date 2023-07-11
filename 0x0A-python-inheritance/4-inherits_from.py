284a4dca15523b6396f002ba84909cf1cbbae3d3
abdelilahboubekri <abdelilah@gmail.com>
2023-07-10 19:40:37 -0700

    inheritance

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Contains the inherits_from function
+"""
+
+
+def inherits_from(obj, a_class):
+    """returns true if obj is a subclass of a_class, otherwise false"""
+     if type(obj) != a_class:
+        return issubclass(type(obj), a_class)
+    else:
+        return False
