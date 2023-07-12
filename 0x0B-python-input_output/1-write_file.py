5925fd08861868ce5a42d4b2960fc72165ea95ad
abdelilahboubekri <abdelilah@gmail.com>
2023-07-11 19:02:26 -0700

    imput output

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,7 +5,6 @@ Module the function "wrtie_file"
 
 
 def write_file(filename="", text=""):
-    """writes a string to a text file (UTF8) and 
-    returns the number of characters written"""
+    """writes a string to a text returns the num of characters written"""
     with open(filename, 'w', encoding='utf=8') as File:
         return File.write(text)
